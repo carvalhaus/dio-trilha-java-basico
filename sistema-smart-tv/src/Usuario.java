983868d425979b3bc43aef5272ef524384ff5957
João Pedro de Carvalho Oliveira <ooliveira.joaop@gmail.com>
2023-02-04 20:55:13 -0300

    Criei o projeto de um sistema de SmartTV, estudando métodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual? " + smartTv.canal);
+        System.out.println("Volume atual? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Novo nível de volume: " + smartTv.volume);
+
+
+    }
+}
