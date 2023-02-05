c77e95a996322c9f3c228440b6af5f6e24a807e5
carvalhaus <ooliveira.joaop@gmail.com>
2023-02-05 17:07:07 -0300

    Desafio Java, Projeto conta bancaria

---
+++ desafio/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args){
+        int numero = args[0];
+        String agencia = args[1];
+        String nomeCliente = args[2];
+        double saldo = args[3];
+
+
+
+    }
+}
