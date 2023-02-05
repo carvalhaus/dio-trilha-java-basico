f689cd667feccf2cbc858c8d9a85c142a24a4b10
carvalhaus <ooliveira.joaop@gmail.com>
2023-02-05 14:11:41 -0300

    estudo operdores lógicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,15 +1,37 @@
 public class Operadores {
     public static void main(String[] args){
-        boolean condicao1 = true;
         
-        boolean condicao2 = false;
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Ciclano";
+        boolean b1 = true;
+        boolean b2 = false;
 
-        if(condicao1 && condicao2){
-            System.out.println("As duas condições são verdadeiras!");
-        }
+        long l1 = 14588L;
+        long l2 = 8997L;
+        byte y1 = 1;
+        short h1 = 25;
 
-        if(condicao1 || condicao2){
-            System.out.println("Uma das duas condições é verdadeira!");
-        }
+        System.out.println("i1 == i2 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+        System.out.println("i1 > i2 " + (i1 > i2));
+        System.out.println("i1 <= i2 " + (i1 <= i2));
+
+        System.out.println("f1 == f2 " + (f1 == f2));
+        System.out.println("f1 != f2 " + (f1 != f2));
+        System.out.println("f1 >= f2 " + (f1 >= f2));
+        System.out.println("f1 < f2 " + (f1 < f2));
+
+        System.out.println("c1 == c2 " + (c1 == c2));
+        System.out.println("c1 != c2 " + (c1 != c2));
+        System.out.println("c1 > c2 " + (c1 > c2));
+        System.out.println("c1 <= c2 " + (c1 <= c2));
     }
 }
