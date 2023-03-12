b7edf59bd8e698b123ebae3ff40dfca04083d2a2
carvalhaus <ooliveira.joaop@gmail.com>
2023-03-12 10:04:49 -0300

    Estudo de collections - List

---
+++ desafio/banco-digital/src/Conta.java
@@ -0,0 +1,19 @@
+public class Conta implements IConta{
+
+    @Override
+    public void sacar(double valor) {
+    }
+
+    @Override
+    public void depositar(double valor) {
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+    }
+
+    @Override
+    public void imprimirExtrator() {
+    }
+    
+}
---
+++ estudos/estudo-POO/estudo-collections/src/lists/ExercicioProposto1.java
@@ -0,0 +1,7 @@
+package lists;
+
+public class ExercicioProposto1 {
+    public static void main(String[] args) {
+        
+    }
+}
